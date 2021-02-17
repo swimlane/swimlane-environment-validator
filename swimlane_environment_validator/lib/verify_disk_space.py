05e99051babd7bb1327809397698939f2793cf49
Bagel <ryan.knauer@swimlane.com>
2021-02-17 12:50:20 -0500

    move back to IEC system

--- swimlane_environment_validator/lib/verify_disk_space.py
+++ swimlane_environment_validator/lib/verify_disk_space.py
@@ -35,7 +35,7 @@ def check_directory_size():
             )
         )
 
-        result['Total Space Size'] = size(total, system=si)
+        result['Total Space Size'] = size(total, system=iec)
         result['Percentage Used'] = int(( ( used / total ) * 100 ))
 
         if total >= minimum_bytes:
