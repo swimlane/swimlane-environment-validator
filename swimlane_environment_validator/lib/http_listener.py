cc84289e641e03b08268746fe5bceabde615d5ca
Bagel <ryan.knauer@swimlane.com>
2021-03-02 00:05:29 -0500

    replace simplehttpserver with flask

--- swimlane_environment_validator/lib/http_listener.py
+++ swimlane_environment_validator/lib/http_listener.py
@@ -3,34 +3,40 @@ import swimlane_environment_validator.lib.config as config
 import swimlane_environment_validator.lib.log_handler as log_handler
 
 from threading import Thread
-from socketserver import ThreadingMixIn
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask
+import click
 import ssl
 
 from OpenSSL import crypto, SSL
 
 logger = log_handler.setup_logger()
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes('{ "status" : "ok" }', 'utf-8'))
-
-    def log_message(self, format, *args):
-        return
-
-class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
-    daemon_threads = True
+app = Flask(__name__)
 
 def serve_on_port(port):
     logger.info("Starting http listener on {}...".format(port))
 
+    app = Flask(__name__)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return '{ "status" : "ok" }'
+
+
+    # Silence the Flask development server logging
+    if config.arguments.debug == False:
+        def secho(text, file=None, nl=None, err=None, color=None, **styles):
+            pass
+
+        def echo(text, file=None, nl=None, err=None, color=None, **styles):
+            pass
+
+        click.echo = echo
+        click.secho = secho
+
     try:
-        server = ThreadingHTTPServer(("",port), Handler)
-        server.socket = ssl.wrap_socket(server.socket, keyfile='./private.key', certfile='./selfsigned.crt', server_side=True)
-        server.serve_forever()
+        app.run(host='0.0.0.0', port=port, ssl_context='adhoc')
     except Exception:
         logger.error('Couldnt start listener on port {}, is something already listening? Ports below 1024 can only be spawned by root.'.format(port))
         logger.debug("Caught exception while starting listener thread ", exc_info=True)
@@ -48,42 +54,3 @@ def start_intra_cluster_listener_threads():
         listener_thread.daemon = True
 
         listener_thread.start()
-
-def cert_gen(
-    emailAddress="emailAddress",
-    commonName="commonName",
-    countryName="NT",
-    localityName="localityName",
-    stateOrProvinceName="stateOrProvinceName",
-    organizationName="organizationName",
-    organizationUnitName="organizationUnitName",
-    serialNumber=0,
-    validityStartInSeconds=0,
-    validityEndInSeconds=10*365*24*60*60,
-    KEY_FILE = "private.key",
-    CERT_FILE="selfsigned.crt"):
-
-    #can look at generated file using openssl:
-    #openssl x509 -inform pem -in selfsigned.crt -noout -text
-    # create a key pair
-    k = crypto.PKey()
-    k.generate_key(crypto.TYPE_RSA, 4096)
-    # create a self-signed cert
-    cert = crypto.X509()
-    cert.get_subject().C = countryName
-    cert.get_subject().ST = stateOrProvinceName
-    cert.get_subject().L = localityName
-    cert.get_subject().O = organizationName
-    cert.get_subject().OU = organizationUnitName
-    cert.get_subject().CN = commonName
-    cert.get_subject().emailAddress = emailAddress
-    cert.set_serial_number(serialNumber)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(validityEndInSeconds)
-    cert.set_issuer(cert.get_subject())
-    cert.set_pubkey(k)
-    cert.sign(k, 'sha512')
-    with open(CERT_FILE, "wt") as f:
-        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
-    with open(KEY_FILE, "wt") as f:
-        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
